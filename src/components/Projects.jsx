231b05bce5b8260d88fa4cf36204cc5a012e52ea
paulinahry <paulinahryszko.de@gmail.com>
2023-03-01 16:47:16 +0100

    project component start

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,43 +1,101 @@
 import React from 'react';
-import ProjectCard from './ProjectCard';
-import cards from './cardData';
 import Link from 'next/link';
+import Image from 'next/image';
+
+// images
+import memorygame from '../assets/projects/memory.png'
+import reactLoginOut from '../assets/projects/start_page.png'
+import uibstart from '../assets/projects/uibstart.png'
+import tipClculator from '../assets/projects/tipCalc.png'
+// icons
+import AiOutlineArrowRight from 'react-icons/ai'
 
 function Projects() {
+
+  const cards = [
+    {
+      title: 'My Personal Portfolio',
+      stack: 'JavaScript, React, Tailwind, Nextjs' ,
+      imgSrc: '',
+      vercel: '',
+      github: ''
+    },
+    {
+      title: 'Tip Calculator ',
+      stack: 'Javascript, React, Tailwind' ,
+      imgSrc: tipClculator,
+      vercel: 'https://tip-calculator-pauhry.vercel.app/',
+      github: 'https://github.com/paulinahry/tip_calculator_pauhry'
+    },
+    {
+      title: 'Login Template',
+      stack: 'JavaScript, React, Firebase' ,
+      imgSrc: reactLoginOut,
+      vercel: 'https://react-login-create-account.vercel.app/',
+      github: 'https://github.com/paulinahry/react-login_createAccount'
+    },
+    {
+      title: 'Memory Game',
+      stack: 'Vanilla JavaScript' ,
+      imgSrc: memorygame,
+      vercel: 'https://programming-basisc-memory-game.vercel.app/',
+      github: 'https://github.com/paulinahry/ProgrammingBasicFinalProjekt'
+    }
+  ]
+  
+
+
   return (
     <section id='projects' className='w-full p-3 flex items-center py-16'>
+
       <div className='max-w-[1240px] mx-auto px-2 py-16'>
         <h3 className='text-m tracking-widest uppercase py-2 border-b text-details border-details'>My projects</h3>
-        <div>
+        <div className='flex flex-wrap justify-center md:flex '>
           {
-          cards?.map((card) => (
-            <div key={card.title}>
-              <img src={card.img} alt='project image' />
+          cards.map((card) => (
+            <div key={card.title}
+              className='mx-2 border rounded mt-4 w-[100%] max-w-[400px] p-2'
+            >
+              <img 
+              src={card.imgSrc} alt='project image' />
               <div>
-                <span>{card.title}</span>
-                <span>{card.stack}</span>
-                <Link href={card.github}>source code</Link>
-                <Link href={card.vercel}>web site</Link>
+
+                <div>
+                  <span className='underline'>Project Name:</span>
+                  {' '}
+                  <span>{card.title}</span>
+                </div>
+
+                <div>
+                  <span className='underline '>Tech Stack:</span>
+                  {' '}
+                  <span>{card.stack}</span>
+                </div>
+
+
+              <div className=' flex justify-around'>
+
+                <Link 
+                  className='underline'
+                  href={card.github}>
+                    source code
+                </Link>
+
+                <Link
+                  className=' underline'
+                  href={card.vercel}>
+                    web site
+                </Link>
+              </div>
+
               </div>
             </div>
           ))
           }
         </div>
-
-        {/* CARDS */}
-        {cards?.map((card) => (
-          <ProjectCard
-            key={card.title}
-            title={card.title}
-            stack={card.stack}
-            img={card.img}
-            deployed={card.deployed}
-            url={card.url}
-          />
-        ))}
       </div>
     </section>
-  );
+  )
 }
 
 export default Projects;
