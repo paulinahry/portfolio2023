542739e26ac03a12dd19591e5054b3a1edcdc6ca
paulinahry <paulinahryszko.de@gmail.com>
2023-03-06 15:16:32 +0100

    msg sended state

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,13 +1,12 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import emailjs from '@emailjs/browser'
-import getConfig from 'next/config'
-
-const { publicRuntimeConfig } = getConfig();
-
+import {SiSpreadshirt} from 'react-icons/si'
 
 
 export const ContactForm = () => {
   const form = useRef()
+  const [isMessaageSent, setIsMessaageSent] = useState(false)
+
 
   const sendEmail = (e) => {
     e.preventDefault()
@@ -18,40 +17,58 @@ export const ContactForm = () => {
       'mw0dY8GAvkKHN5IjE'
     )
       .then((result) => {
-          console.log('msg ')
-          console.log(result.text)
+          console.log('msg sent: ',result.text)
+          setIsMessaageSent(true)
           e.target.reset()
       }, (error) => {
-          console.log(error.text)
-          console.log('error')
+          console.log('error',error.text)
       })
   }
 
   return (
-    <form ref={form} 
-    onSubmit={sendEmail}
-    className='m-4 flex flex-col 
-    md:w-[40%] justify-center'
-    >
-      <label>Name</label>
-      <input 
-      className='text-black'
-      type="text" name="user_name" />
+    <section className='flex justify-center'>
 
-      <label>Email</label>
-      <input 
-      className='text-black'
-      type="email" name="user_email" />
+      <form ref={form} 
+      onSubmit={sendEmail}
+      className='m-4 flex flex-col w-[80%]
+      md:w-[55%] justify-center'
+      >
 
-      <label>Message</label>
-      <textarea 
-      className='text-black'
-      name="message" />
+        <label>Name</label>
+        <input 
+        className='text-black rounded-sm' type="text" required
+        name="user_name" />
 
-      <input 
-      className='border px-2 rounded cursor-pointer w-[180px] mt-2'
-      type="submit" 
-      value="Send" />
-    </form>
-  );
-};
+
+        <label>Email</label>
+        <input 
+        className='text-black  rounded-sm' required type="email" 
+        name="user_email" />
+
+
+        <label>Message</label>
+        <textarea 
+        className='text-black  rounded-sm' required name="message" />
+
+
+        <div className='flex flex-col items-end'>
+
+        <input 
+        className='border px-2 rounded cursor-pointer w-[180px] mt-2'
+        type="submit" 
+        value="Send" />
+
+        </div>
+        {
+        isMessaageSent && (
+        <div className='text-details text-sm flex'>
+
+          <SiSpreadshirt 
+          className='pr-1'
+          size={19}/> <p  >Thank you, message delivered.</p> 
+        </div> 
+        )} 
+      </form>
+    </section>
+  )
+}
