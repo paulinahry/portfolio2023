74e87a365d014a302ebdefb5d91a935c9fa5d011
paulinahry <paulinahryszko.de@gmail.com>
2023-03-11 13:38:34 +0100

    progress bar loading new

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 import VisibilitySensor from 'react-visibility-sensor';
@@ -7,16 +7,18 @@ function AnimatedCircularProgressbar({ percentage }) {
     const [animationStart, setAnimationStart] = useState(false);
   
     const handleVisibilityChange = (isVisible) => {
-      isVisible && setAnimationStart(true)
+       setAnimationStart(isVisible)
     }
 
-
+    // progress bar styling
     const progressBarStyle = buildStyles({
       pathColor: '#4ff7b2',
       textColor: '#4ff7b2',
       trailColor: '#0B4147',
     })
-  
+
+
+
     return (
       <VisibilitySensor onChange={handleVisibilityChange} partialVisibility={true}>
         <div className="w-[80px] h-[80px] sm:w-[100px] sm:h-[100px] md:w-[120px] md:h-[120px]">
